867227983d3f1274a0404b8e644a36779ac58d4e
darasaikumar035 <saikumardhara923@gmail.com>
2024-04-14 19:55:17 +0530

    Start

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+#title
+st.title("welcome Dara")
+#Header-
+st.header("MAN OF MASSEs")
+#sub header
+st.subheader("Sai")
+#to give information
+st.info("From Madanapalle")
+#for warming
+st.warning("Highvoltage")
+#write any text
+st.write("Jungle Man")
+
+
+st.latex(r'''a+b X^2 ''')
